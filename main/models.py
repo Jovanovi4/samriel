3a5568ab1da1a0aca3b1f41298aa4c74d8905292
Jovanovich <mihailup56@gmail.com>
2024-08-05 00:33:37 +0400

    v.01

--- main/models.py
+++ main/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Building(models.Model):
+
+    CATEGORY_CHOICES = [
+        ('apartment', 'Квартира'),
+        ('house', 'Дом'),
+        ('dacha', 'Дача'),
+        ('land', 'Земельный участок'),
+    ]
+        
+    title = models.CharField('Наименование объекта', max_length=255) 
+    top_description = models.TextField('Текст над названием', blank=True, null=True) 
+    down_description = models.TextField('Текст под названием', blank=True, null=True) 
+    deadline = models.CharField('Срок сдачи', max_length=255, blank=True, null=True)
+    image = models.ImageField(blank=True, null=True, upload_to='images/')
+    jarlyk = models.CharField('Ярлык', max_length=255, blank=True, null=True)
+    about_before_slide = models.TextField('Описание до слайдера', blank=True, null=True)
+    about_after_slide = models.TextField('Описание после слайдера', blank=True, null=True)
+    category = models.CharField('Категория недвижимости', max_length=100, choices=CATEGORY_CHOICES, default='house', blank=True)
+    video_name = models.CharField('Заголовок видео', max_length=255, blank=True, null=True) 
+    video = models.CharField('Видео с YouTube', max_length=255, blank=True, null=True) # Ссылка на видео
+    location = models.CharField('Координаты на карте', max_length=255, blank=True, null=True)
+    adres = models.CharField('Адрес с текстом', max_length=255, blank=True, null=True)
+    characteristic = models.CharField('Характеристика', max_length=255, blank=True, null=True)
+    description = models.TextField('Описание', blank=True, null=True)
+    advantages = models.CharField('Преимущество', max_length=255, blank=True, null=True)
+    deteil_description = models.TextField('Детальное описание', blank=True, null=True)
+    square = models.IntegerField('Площадь', blank=True, null=True)
+    floor = models.IntegerField('Этаж', blank=True, null=True) 
+    all_floor = models.IntegerField('Всего этажей', blank=True, null=True)
+    price = models.IntegerField('Цена объекта', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
--- main/views.py
+++ main/views.py
@@ -1,11 +1,53 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
+# views.py
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Building
+from .forms import BuildingForm
 
 def index(request):
-    context = {
-        'objects': 'Недвижимость',
-        'content': 'fawfов'
-    }
-    return render(request, 'main/index.html', context)
+    buildings = Building.objects.all()
+    return render(request, 'main/index.html', {'buildings': buildings})
+
+def add(request):
+    add_form = BuildingForm()
+
+    form = None
+
+    if request.method == 'POST':
+        form_type = request.POST.get('type')
+        if form_type == '1':
+            form = BuildingForm(request.POST, request.FILES)
+        elif form_type == '2':
+            form = BuildingForm(request.POST, request.FILES)
+
+        if form and form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form_type = request.GET.get('type')
+        if form_type == '1':
+            form = BuildingForm()
+        elif form_type == '2':
+            form = BuildingForm()
+
+    return render(request, 'main/add.html', {
+        'add_form': add_form,
+        'form': form,
+    })
+
+def edit(request, pk):
+    building = get_object_or_404(Building, pk=pk)
+    if request.method == 'POST':
+        form = BuildingForm(request.POST, request.FILES, instance=building)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = BuildingForm(instance=building)
+    return render(request, 'main/edit.html', {'form': form, 'building': building})
+
+def delete(request, pk):
+    building = get_object_or_404(Building, pk=pk)
+    if request.method == 'POST':
+        building.delete()
+        return redirect('index')
+    return render(request, 'main/confirm_delete.html', {'building': building})
